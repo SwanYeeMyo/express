f19fdd861a3062de0756be68c6a4e762e3549273
Swan Yee Myo <swanyeemyo2001@gmail.com>
2025-10-17 14:32:42 +0630

    setup schema and jwt

--- src/model/user.js
+++ src/model/user.js
@@ -1,5 +1,8 @@
 import mongoose, { Schema } from "mongoose";
 import paginate from "mongoose-paginate-v2";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 const userSchema = new Schema(
   {
     username: {
@@ -34,6 +37,33 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+//pre middleware
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  //using hash change the plain password to hash
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+//custom methood
+userSchema.methods.isPasswordisMatch = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+//generate Token
+userSchema.methods.generateAccessToken = async function () {
+  jwt.sign(
+    {
+      _id: this_id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.JWT_SECRET
+  ),
+    { expiresIn: 60 * 60 };
+};
 
 userSchema.plugin(paginate);
 
